e904bc34d6d93f0c710c6aac5c0d99f824a34c21
Kevin Stephens <kevinmstephens@gmail.com>
2014-06-04 23:51:09 -0700

    Added mongoDB and created test API to server temperature data, working but not finished

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var http = require('http');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+
+var tempHourStaRoutes = require('./api/routes/tempHourStaRoutes');
+
+mongoose.connect('mongodb://localhost/users');
 
 
 var app = express();
-
+app.use(bodyparser.json());
 app.use(express.static(__dirname + '/dist'));
-
 app.set('port', process.env.PORT || 3000);
+
+app.get('/api/v0_0_1/temp', tempHourStaRoutes.collection);
 
 var server = http.createServer(app);
 

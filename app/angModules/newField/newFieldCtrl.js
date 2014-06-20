ac150e2c59c9573bd01b09a560d1eb3d7fd7cafb
Kyle Warbis <kyle.warbis@gmail.com>
2014-06-19 21:00:11 -0700

    added form stuff and angular file structure

---
+++ app/angModules/newField/newFieldCtrl.js
@@ -0,0 +1,19 @@
+module.exports.newFieldCtrl = function($scope){
+	$scope.master = {};
+
+	$scope.reset = function(){
+		$scope.field = angular.copy($scope.master);
+	};
+
+	$scope.isUnchanged = function(){
+		return angular.equals(field, $scope.master);
+	};
+
+	$scope.update = function(field){
+		$scope.master=angular.copy(field);
+		console.log($scope.master);
+		return $scope.master;
+	};
+
+	$scope.reset();
+};

f3e69ceb71c3121f33f901ecd5d62b02a889a548
Kyle Warbis <kyle.warbis@gmail.com>
2014-06-22 12:45:23 -0700

    added new field to newField Temp

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@ var getDailyHighLow = require('./getDailyHighLow');
 var makeDailyHighLowGraph = require('./makeDailyHighLowGraph');
 
 $(function () {
+	console.log("where the graph at?");
 	makeDailyHighLowGraph(235, 1-0-2014, 5-0-2014);
 });
 

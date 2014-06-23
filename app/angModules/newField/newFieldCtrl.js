2636ae246370ff3e03ce38c17550ca5f027c4541
Kyle Warbis <kyle.warbis@gmail.com>
2014-06-23 11:23:28 -0700

    catching up to master, modularizing angular view

--- app/angModules/newField/newFieldCtrl.js
+++ app/angModules/newField/newFieldCtrl.js
@@ -1,32 +1,5 @@
-(function(){
-	var formMod = angular.module('formMod', []);
-	
-	formMod.controller('newFieldCtrl', function($scope){
-		$scope.master = {};
-	
-		$scope.reset = function(){
-			$scope.field = angular.copy($scope.master);
-		};
-	
-		$scope.isUnchanged = function(){
-			return angular.equals(field, $scope.master);
-		};
-	
-		$scope.update = function(field){
-			$scope.master=angular.copy(field);
-			console.log($scope.master);
-			return $scope.master;
-		};
-	
-		$scope.reset();
-	});
-})();
 
-
-
-
-
-/*module.exports.newFieldCtrl = function($scope){
+module.exports.newFieldCtrl = function($scope){
 	$scope.master = {};
 
 	$scope.reset = function(){
@@ -44,4 +17,4 @@
 	};
 
 	$scope.reset();
-};*/
+};

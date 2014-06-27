8dc8e962ecb2212fb40ad4c821d77c77f77487e3
Kevin Stephens <kevinmstephens@gmail.com>
2014-06-27 01:23:36 -0700

    Fixed inverted GDD cumul

--- app/js/runGddCalc.js
+++ app/js/runGddCalc.js
@@ -1,8 +1,5 @@
 module.exports = function runGddCalc (tempData, currentPlanting, currentFarmer) {
 
-  console.log(" ");
-  console.log("Hitting inside GDD CALC for PlantingID: " + currentPlanting._id);
-
   var tMax = 0;
   var tMin = 0;
   var currentMax = 0;
@@ -14,45 +11,53 @@ module.exports = function runGddCalc (tempData, currentPlanting, currentFarmer) 
   var currentDate = new Date(); // sets to current date
   var farmer = currentFarmer;
 
+  tempData = tempData.reverse();
+
+console.log(tempData[10]);
   for (var k=0; k<tempData.length; k++) {
-    dateOutput.push(tempData[k].calendar_date);
-    var gddValue = 0;
+    if (tempData[k].daily_max){
+      dateOutput.push(tempData[k].calendar_date);
+      var gddValue = 0;
 
-    currentMax = parseFloat(tempData[k].daily_max);
-    currentMin = parseFloat(tempData[k].daily_min);
+      currentMax = parseFloat(tempData[k].daily_max);
+      currentMin = parseFloat(tempData[k].daily_min);
 
-    if (currentMax > currentPlanting.gddParameters.maxTempF) {
-      tMax = currentPlanting.gddParameters.maxTempF;
+      if (currentMax > currentPlanting.gddParameters.maxTempF) {
+        tMax = currentPlanting.gddParameters.maxTempF;
+      }
+      else tMax = currentMax;
+
+      if (currentMin < currentPlanting.gddParameters.minTempF) {
+        tMin = currentPlanting.gddParameters.minTempF;
+      }
+      else tMin = currentMin;
+
+      if (currentMax < currentPlanting.gddParameters.minTempF) {
+        tMax = currentPlanting.gddParameters.minTempF;
+      }
+
+
+      gddValue = (tMax + tMin)/2 - currentPlanting.gddParameters.minTempF;
+
+      //console.log('gdd val', gddValue);
+      //DEBUG FOR NEGATIVE GDD VALUES:
+      // if(gddValue > 0) {
+      //   console.log("DATE: " + tempData[k].calendar_date);
+      //   console.log("TMIN: " + tMin);
+      //   console.log("TMAX: " + tMax);
+      //   console.log("tempData[k].daily_max: " + tempData[k].daily_max);
+      //   console.log("currentMax: " + currentMax);
+      //   console.log("ParamMAX: " + currentPlanting.gddParameters.maxTempF);
+      //   console.log("TBASE: " + currentPlanting.gddParameters.minTempF);
+      //   console.log("GDD: " + gddValue);
+      // }
+
+      if (gddValue){
+        currentCum += gddValue;
+      }
+      gddOutput.push(gddValue);
+      gddCum.push(currentCum);
     }
-    else tMax = currentMax;
-
-    if (currentMin < currentPlanting.gddParameters.minTempF) {
-      tMin = currentPlanting.gddParameters.minTempF;
-    }
-    else tMin = currentMin;
-
-    if (currentMax < currentPlanting.gddParameters.minTempF) {
-      tMax = currentPlanting.gddParameters.minTempF;
-    }
-
-    gddValue = (tMax + tMin)/2 - currentPlanting.gddParameters.minTempF;
-
-    //DEBUG FOR NEGATIVE GDD VALUES:
-    // if(gddValue > 0) {
-    //   console.log("DATE: " + tempData[k].calendar_date);
-    //   console.log("TMIN: " + tMin);
-    //   console.log("TMAX: " + tMax);
-    //   console.log("tempData[k].daily_max: " + tempData[k].daily_max);
-    //   console.log("currentMax: " + currentMax);
-    //   console.log("ParamMAX: " + currentPlanting.gddParameters.maxTempF);
-    //   console.log("TBASE: " + currentPlanting.gddParameters.minTempF);
-    //   console.log("GDD: " + gddValue);
-    // }
-
-    currentCum += gddValue;
-    console.log("CUM: " + currentCum);
-    gddOutput.push(gddValue);
-    gddCum.push(currentCum);
   }
 
   var update  = { lastUpdated : currentDate };
@@ -61,7 +66,7 @@ module.exports = function runGddCalc (tempData, currentPlanting, currentFarmer) 
   //farmer.findByIdAndUpdate(farmer._id, );
 
 
-  var output = {date: dateOutput, gdd: gddOutput.reverse(), cum: gddCum.reverse()};
+  var output = {date: dateOutput, gdd: gddOutput, cum: gddCum};
   return output;
 
 };

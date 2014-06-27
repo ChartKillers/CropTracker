d4714b9d229ecc19bcd504175df8f2e6c5246b38
Kevin Stephens <kevinmstephens@gmail.com>
2014-06-26 21:43:30 -0700

    daily temp fix

--- app/js/graphs/makeDailyHighLowGraph.js
+++ app/js/graphs/makeDailyHighLowGraph.js
@@ -9,6 +9,9 @@ module.exports = function makeDailyHighLowGraph(stationId, startDate, endDate){
 	
 	getDailyHighLow(stationId, startDate, endDate, function (tempJSON) {
 
+        console.log("TEMP DATA RAW");
+        console.log(tempJSON);
+
 		parseDailyHighLowToGraph(tempJSON, function (parsedTempJSON) {
 
 			paintDailyHighLowGraph(parsedTempJSON);
